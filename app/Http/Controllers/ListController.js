b1ac483b0bfc667146603e5754846d719dd4a3c8
Onete Bogdan <onete.bogdan.93@gmail.com>
2016-05-05 21:58:56 +0300

    lista de anime

--- app/Http/Controllers/ListController.js
+++ app/Http/Controllers/ListController.js
@@ -5,19 +5,24 @@ const List = use('App/Model/Anime'),
 
 class ListController {
   *library (request, response) {
-
+    const userId =  yield request.session.get('user_id')
+    if(userId) {
       const anime = yield Anime.all()
       if (anime) {
         const titleAnime = (yield anime.title().fetch()).value()
         const englishTitleAnime = (yield anime.english_title().fetch()).value()
         const descriptionAnime = (yield anime.description().fetch()).value()
-        ListService.insertDisplayTitles(userAnime)
+        ListService.insertDisplayTitles(titleAnime)
+        ListService.insertDisplayEnglishTitles(englishTitleAnime)
+        ListService.insertDisplayDescriptions(descriptionAnime)
         yield response.sendView('animedatabase', {anime: anime.attributes})
       } else {
-        forget('anime')
+        forget('user_id')
         response.redirect('/mainpage')
       }
-
+    } else {
+        response.redirect('/mainpage')
+      }
   }
 }
 
