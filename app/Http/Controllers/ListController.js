ab6a6afaa02b312ff8b9e168a40f0182e96262d3
Onete Bogdan <onete.bogdan.93@gmail.com>
2016-05-06 17:10:48 +0300

    pushing

--- app/Http/Controllers/ListController.js
+++ app/Http/Controllers/ListController.js
@@ -1,28 +1,15 @@
 'use strict'
 
-const List = use('App/Model/Anime'),
-  ListService = use("App/Services/ListService")
+const Anime = use('App/Model/Anime'),
+  AnimeService = use("App/Services/AnimeService")
 
 class ListController {
-  *library (request, response) {
-    const userId =  yield request.session.get('user_id')
-    if(userId) {
-      const anime = yield List.all()
-      if (anime) {
-        const titleAnime = (yield anime.title().fetch()).value()
-        const englishTitleAnime = (yield anime.english_title().fetch()).value()
-        const descriptionAnime = (yield anime.description().fetch()).value()
-        ListService.insertDisplayTitles(titleAnime)
-        ListService.insertDisplayEnglishTitles(englishTitleAnime)
-        ListService.insertDisplayDescriptions(descriptionAnime)
-        yield response.sendView('animedatabase', {anime: anime.attributes})
-      } else {
-        forget('user_id')
-        response.redirect('/mainpage')
-      }
-    } else {
-        response.redirect('/mainpage')
-      }
+  *animedatabase (request, response) {
+    const anime = (yield Anime.all()).value()
+    if (anime) {
+      AnimeService.insertDisplayTitles(anime)
+      yield response.sendView('animedatabase', {anime: anime})
+    }
   }
 }
 
