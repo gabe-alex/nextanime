be1fcb365e9bf99c8baf6099b37179562e40a0f1
Onete Bogdan <onete.bogdan.93@gmail.com>
2016-05-13 03:31:26 +0300

    whatever

--- app/Services/AnimeService.js
+++ app/Services/AnimeService.js
@@ -12,6 +12,62 @@ class AnimeService {
     return anime
   }
 
+  static getDisplayEnglishTitle(anime) {
+    return anime.english_title
+  }
+
+  static insertDisplayEnglishTitle(anime) {
+    anime.display_english_title = AnimeService.getDisplayEnglishTitle(anime)
+    return anime
+  }
+
+  static getDisplayAuthor(anime) {
+    return anime.author
+  }
+
+  static insertDisplayAuthor(anime) {
+    anime.display_author = AnimeService.getDisplayAuthor(anime)
+    return anime
+  }
+
+  static getDisplayStudio(studios) {
+    return studios.name
+  }
+
+  static insertDisplayStudio(studios) {
+    studios.display_studio = AnimeService.getDisplayStudio(studios)
+    return studios
+  }
+
+  static getDisplayAnimeDescription(anime) {
+    return anime.description
+  }
+
+  static insertDisplayAnimeDescription(anime) {
+    anime.display_description = AnimeService.getDisplayAnimeDescription(anime)
+    return anime
+  }
+
+  static getDisplayCharacters(characters) {
+    return characters.romaji_name
+  }
+
+  static insertDisplayCharacters(characters) {
+    characters.display_title = AnimeService.getDisplayCharacters(characters)
+    return characters
+  }
+
+  static getDisplayCharacters(characters) {
+    return characters.romaji_name
+  }
+
+  static insertDisplayCharacters(characters) {
+    characters.display_title = AnimeService.getDisplayCharacters(characters)
+    return characters
+  }
+
+
+
   static insertDisplayTitles(animeList) {
     return _(animeList).map(function(item) {return AnimeService.insertDisplayTitle(item)}).value()
   }
