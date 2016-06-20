5125aebad19e567509bf2639cc8ac56c2c6ad077
Gabriel Alecu <gc.alecu@gmail.com>
2016-06-20 04:34:53 +0300

    Better handling of PUBLIC_PORT

--- config/fb.js
+++ config/fb.js
@@ -2,9 +2,11 @@
 
 const Env = use('Env');
 
+const port = Env.get('PUBLIC_PORT', 80);
+
 // facebook app settings
 module.exports = {
   appID: Env.get('FB_APPID'),
   appSecret: Env.get('FB_APPSECRET'),
-  callbackUrl : 'http://'+Env.get('PUBLIC_HOST')+':'+Env.get('PUBLIC_PORT', 80)+'/login_fb_callback'
+  callbackUrl : 'http://'+Env.get('PUBLIC_HOST')+(port==80 && '' || ':'+port)+'/login_fb_callback'
 };
