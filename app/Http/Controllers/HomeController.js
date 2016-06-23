e7b2887217b7cf16ed402338200073ab406c5d63
Gabriel Alecu <gc.alecu@gmail.com>
2016-06-23 18:33:36 +0300

    Fixed error when user not logged in.

--- app/Http/Controllers/HomeController.js
+++ app/Http/Controllers/HomeController.js
@@ -10,12 +10,12 @@ class HomeController {
     const userId =  yield request.session.get('user_id');
     if(userId) {
       user = yield User.find(userId);
-      recs = yield Recommendation.getRecommendations(user, 5);
+      recs.value() = yield Recommendation.getRecommendations(user, 5);
     }
     const top = yield Recommendation.getTopAnime(5);
     const newAnime = yield Anime.query().orderBy('created_at','desc').limit(5).fetch();
 
-    yield response.sendView('home', {user: user, recs: recs.value(), top: top.value(), new: newAnime.value()});
+    yield response.sendView('home', {user: user, recs: recs, top: top.value(), new: newAnime.value()});
   }
 }
 
